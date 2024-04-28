94e2185edbb8482d3f033dd45ac1653d5e856cc1
David Hall <personal@davesoffice.io>
2024-04-28 02:38:05 -0500

    mutation intro

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,11 +1,25 @@
-import {helloName} from "./index";
+import { calculateBMI } from './index';
 
-describe("index.ts", () => {
+describe('calculateBMI', () => {
 
-    it("should pass", () => {
-        const name = "John";
-        const result = helloName(name);
-        expect(result).toBe("hello, John!");
-    })
+    it('should return "Underweight" for a person with weight 50kg and height 1.75m', () => {
+        const result = calculateBMI(50, 1.75);
+        expect(result).toBe('Underweight');
+    });
 
-})
+    it('should return "Normal weight" for a person with weight 70kg and height 1.75m', () => {
+        const result = calculateBMI(70, 1.75);
+        expect(result).toBe('Normal weight');
+    });
+
+    it('should return "Overweight" for a person with weight 90kg and height 1.75m', () => {
+        const result = calculateBMI(90, 1.75);
+        expect(result).toBe('Overweight');
+    });
+
+    it('should return "Obesity" for a person with weight 140kg and height 1.75m', () => {
+        const result = calculateBMI(140, 1.75);
+        expect(result).toBe('Obesity');
+    });
+
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 
-
-export function helloName(name: string) {
-    return `hello, ${name}!`;
+export function calculateBMI(weight: number, height: number): string {
+    const bmi: number = weight / (height * height);
+    if (bmi < 18.5) {
+        return 'Underweight';
+    } else if (bmi < 24.9) {
+        return 'Normal weight';
+    } else if (bmi < 29.9) {
+        return 'Overweight';
+    } else {
+        return 'Obesity';
+    }
 }
